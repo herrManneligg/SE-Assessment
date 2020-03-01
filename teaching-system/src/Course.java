d85a8bb8071934a95bb4dc9993b05ee09b4ed8ab
2444965H <59885910+2444965H@users.noreply.github.com>
2020-03-01 22:05:34 +0000

    Added Testing class "MethodSimulation"
    - Added testing class "MethodSimulation"
    - Corrected some method in Course class and ListOfRequirements class

--- teaching-system/src/Course.java
+++ teaching-system/src/Course.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Course {
 
 	private String name;
@@ -11,37 +13,51 @@ public class Course {
 		requirements = null;
 	}
 
-	public void update(String name, Integer timeExp, String availability) { //updates name, experience, and availability
-		this.name = name;
-		if (this.requirements == null) {
-			this.requirements = new ListOfRequirements(this, timeExp, availability);
-		} else {
-			if (timeExp == null) {
-				this.requirements.update(availability);
-			}else if (availability == null) {
-				this.requirements.update(timeExp);
-			}else {
-				this.requirements.update(timeExp, availability);
-			}
-		}
-	}
-
 	public String getCourseInfo() {
 
 		String info = "- Course: " + name + "\n";
 
 		if (assignedTeacher == null) {
-			info = info + "Teacher: Not assigned";
+			info = info + "- Teacher: Not assigned" + "\n";
 		} else {
 			info = info + "- Teacher: " + assignedTeacher.getName() + "\n";
 		}
 
-		if (approved = true) {
+		if (approved == true) { //corrected from = to ==
 			info = info + "- Approved for teaching: Yes" + "\n";
 		} else {
 			info = info + "- Approved for teaching: No" + "\n";
 		}
 		return info;
+	}
+
+	public String getCourseName() {
+		return name;
+	}
+
+	public void update(String name, Integer timeExp, String availability) { // updates name, experience, and
+																			// availability
+		if (name == null) {
+			// no update of the name
+		} else {
+			this.name = name;
+		}
+
+		if ((timeExp == null) && (availability == null)) {
+			// only Course-Name gets updated, no changes to the ListOfRequirements
+		} else {
+			if (this.requirements == null) {
+				this.requirements = new ListOfRequirements(this, timeExp, availability);
+			} else {
+				if (timeExp == null) {
+					this.requirements.update(availability);
+				} else if (availability == null) {
+					this.requirements.update(timeExp);
+				} else {
+					this.requirements.update(timeExp, availability);
+				}
+			}
+		}
 	}
 
 	public void assingTeacher(Teacher t) {
@@ -56,25 +72,31 @@ public class Course {
 		this.approved = false;
 	}
 
-	public Teacher getTeacher() { //enables method to return null value
+	public Teacher getTeacher() { // enables method to return null value
 		if (this.assignedTeacher == null) {
-			return null;
+			Teacher t = null;
+			return t;
 		} else {
 			return assignedTeacher;
 		}
 	}
 
-	public String getCourseName() {
-		return name;
-	}
-	
-	public String readList() { //gives back String
+	public String readList() { // gives back String
 		String info = "";
-		if(this.requirements == null) {
-			
-		}else {
-			info = "The Course-Requirements are: " + this.requirements.getTimeExp() + " years of experience and it will be available in the" + this.requirements.getAvailability();
+		if (this.requirements == null) {
+			info = "- No Course-Requirements are currently associated with this course" + "\n";
+		} else {
+			info = "- The Course-Requirements are: " + this.requirements.getTimeExp()
+					+ " years of experience and it will be available in the " + this.requirements.getAvailability()
+					+ "\n";
 		}
-	return info;
+		return info;
 	}
+
+	public void assignRequirementsList(ListOfRequirements LoR) {
+		this.requirements = LoR;
+		LoR.setCorrespondingCourse(this);
+
+	}
+
 }
